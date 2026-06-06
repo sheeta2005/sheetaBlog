b93539607cc5c99f06125f5d4e66a29cc5ad83aa
sheeta1998 <2169298883@qq.com>
2026-06-06 17:27:08 +0800

    二次元版本

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import './style.css'
+import parallax from './parallax.js'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp?.(ctx)
+    parallax.enhanceApp(ctx)
+  }
+}
